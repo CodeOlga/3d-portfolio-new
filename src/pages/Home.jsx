c441331b4b534911b2da5e867c66744b2abfa0b3
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-09-07 12:21:47 +0300

    pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+
+import Loader from '../components/Loader'
+import Island from '../models/Island'
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+  POPUP
+</div> */}
+
+const Home = () => {
+   const adjustBiplaneForScreenSize = () => {
+     let screenScale = null;
+     let screenPosition = [0, -6.5, -43];
+     let rotation = [0.1, 4.7, 0];
+
+    // If screen width is less than 768px, adjust the scale and position
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+  // const adjustBiplaneForScreenSize = () => {
+  //   let screenScale, screenPosition;
+
+  //   // If screen width is less than 768px, adjust the scale and position
+  //   if (window.innerWidth < 768) {
+  //     screenScale = [1.5, 1.5, 1.5];
+  //     screenPosition = [0, -1.5, 0];
+  //   } else {
+  //     screenScale = [3, 3, 3];
+  //     screenPosition = [0, -4, -4];
+  //   }
+
+  //   return [screenScale, screenPosition];
+  // };
+
+  const [islandScale, islandPosition, islandRotation] = adjustBiplaneForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className='w-full h-screen bg-transparent'
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        {/* Suspense using like a wrapper, for loading screen */}
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  )
+}
+
+export default Home
