c75aee043e04e6f3f1e455ce04e0993457dc1b1b
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-09-08 11:03:01 +0300

    island 3d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 
 import Loader from '../components/Loader'
 import Island from '../models/Island'
+import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
 </div> */}
 
 const Home = () => {
-   const adjustBiplaneForScreenSize = () => {
-     let screenScale = null;
-     let screenPosition = [0, -6.5, -43];
-     let rotation = [0.1, 4.7, 0];
+  const [isRotating, setIsRotating] = useState(false)
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
 
     // If screen width is less than 768px, adjust the scale and position
     if (window.innerWidth < 768) {
@@ -23,41 +28,53 @@ const Home = () => {
 
     return [screenScale, screenPosition, rotation];
   };
-  // const adjustBiplaneForScreenSize = () => {
-  //   let screenScale, screenPosition;
 
-  //   // If screen width is less than 768px, adjust the scale and position
-  //   if (window.innerWidth < 768) {
-  //     screenScale = [1.5, 1.5, 1.5];
-  //     screenPosition = [0, -1.5, 0];
-  //   } else {
-  //     screenScale = [3, 3, 3];
-  //     screenPosition = [0, -4, -4];
-  //   }
+const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
 
-  //   return [screenScale, screenPosition];
-  // };
+    // If screen width is less than 768px, adjust the scale and position
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4]
+    }
 
-  const [islandScale, islandPosition, islandRotation] = adjustBiplaneForScreenSize();
+    return [screenScale, screenPosition];
+  };
 
+  
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
+  
   return (
     <section className="w-full h-screen relative">
       <Canvas
-        className='w-full h-screen bg-transparent'
+        className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}
       >
         {/* Suspense using like a wrapper, for loading screen */}
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
-
+          <directionalLight position={[1, 1, 1]} intensity={2}/>
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1} />
+          
+          <Bird />
+          <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+          />
+          <Plane
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+            isRotating={isRotating}
           />
         </Suspense>
       </Canvas>
